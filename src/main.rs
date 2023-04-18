162e78143656d10317b43b1bf4747aa2e9d55224
Rene D. Obermueller <cmdrrdo@gmail.com>
2023-04-18 22:01:31 +0200

    add hotkey

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use gtk::prelude::*;
+use debug_print::debug_println;
+
+use gtk::{gdk::ModifierType, prelude::*};
 
 /*
 false warning, impl code generate via macro
@@ -17,10 +19,22 @@ fn main() {
         Inhibit(false)
     });
 
-   /*  window.connect_key_press_event(move |_, _| {
-        gtk::main_quit();
+    window.connect_key_press_event(move |_: &Window, eventkey| {
+        debug_println!("{:?} {:?}", eventkey.state(), eventkey.keycode().unwrap());
+
+
+        if eventkey.state().contains(ModifierType::CONTROL_MASK) {
+            // this is possibly extended in the future ;)
+            #[allow(clippy::single_match)]
+            match eventkey.keycode().unwrap() {
+                24 => {
+                    gtk::main_quit();
+                }
+                _ => {}
+            }
+        }
         Inhibit(false)
-    }); */
+    });
 
     window.show_all();
 

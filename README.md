a1b709fdacbe72dbc432cbc0347bf0d048eb6201
Rene D. Obermueller <cmdrrdo@gmail.com>
2025-05-28 22:59:18 +0200

    update to GTK4

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## What is this?
 
-Creates a blank window using gtk. On my 43" screen, tiled windows can create visual challenges. Rather than having the window float, I pick up some screen real estate using a blank window. In the past, I've started terminals for the same purpose and that's OK too, of course.
+Creates a blank window using gtk. On my 43" 4k screen, tiled windows can create visual challenges. Rather than having the window float, I pick up some screen real estate using a blank window. In the past, I've started terminals for the same purpose and that's OK too, of course.
 
 ![Screenshot](/img/20230517_213216.png)
   
@@ -9,6 +9,12 @@ I'm using the following keybinding in my sway config:
 ```sway
 bindsym $mod+Shift+Return exec exec blankwindow-gtk-rs
 ```
+
+### Why no `smart_gaps` or `gaps`?
+
+I feel this depends on the screen used. With an ultra wide screen, I tend to prefer hotkeys for setting outer gaps on the fly. That said, I'm still finding the window useful for shadowing floating game windows.
+
+![Screenshot](/img/20250528_225344.png)
 
 ## Styling
 
@@ -23,6 +29,13 @@ cargo install --path .
 to install to `$HOME/.cargo`.  
   
 Or just use `cargo build` or `cargo build --release` to produce binaries beneath `target/` and copy manually if needed.
+
+## Run
+
+`blankwindow-gtk-rs [--app-id <app_id>]`
+
+Note that `app_id` needs to be a valid gtk application id, e.g. `com.github.RobertMueller2.blankwindow` or something.
+
 
 ## Transparency while testing
 
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,82 @@
+#[allow(unused_imports)]
+use debug_print::debug_println;
 use std::fs;
 
-use debug_print::debug_println;
+use gtk::{
+    CssProvider, EventControllerKey,
+    Orientation::Vertical,
+    gdk::{self, ModifierType},
+    prelude::*,
+};
 
-use gtk::{gdk::{self, ModifierType}, CssProvider, Orientation::Vertical, Window, WindowType, prelude::*};
+fn usage(exe: String) {
+    println!("{:?} --help", exe);
+    println!("{:?} --app-id <app id>", exe);
+}
 
-fn main() {
+fn main() -> glib::ExitCode {
+    let args: Vec<String> = std::env::args().collect();
+    let mut args_gtk: Vec<String> = Vec::new();
+    let exe = args[0].clone();
+    let mut app_id = "com.github.RobertMueller2.blankwindow-gtk-rs";
 
-    gtk::init().expect("Couldn't initialize GTK");
-
-    let window = gtk::Window::new(WindowType::Toplevel);
-    let vbox = gtk::Box::new(Vertical, 0);
-    
-    window.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        gtk::glib::Propagation::Proceed
-    });
-
-    let user_css_path = dirs::config_dir().unwrap().join("blankwindow.css");
-
-    let css_content = fs::read_to_string(user_css_path).unwrap_or("".to_owned());
-    let css_bytes = match css_content.as_str() {
-        "" => include_bytes!("default-style.css"),
-        x => x.as_bytes()
-    };
-    
-    let css_provider = CssProvider::new();
-    css_provider.load_from_data(css_bytes).expect("Failed to load CSS");
-    gtk::StyleContext::add_provider_for_screen(
-        &gdk::Screen::default().expect("Error initializing gtk css provider"),
-        &css_provider,
-        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION
-    );
-
-    window.connect_key_press_event(move |_: &Window, eventkey| {
-        debug_println!("{:?} {:?}", eventkey.state(), eventkey.keycode().unwrap());
-
-
-        if eventkey.state().contains(ModifierType::CONTROL_MASK) {
-            // this is possibly extended in the future ;)
-            #[allow(clippy::single_match)]
-            match eventkey.keycode().unwrap() {
-                24 => {
-                    gtk::main_quit();
-                }
-                _ => {}
+    let mut i = 1;
+    while i < args.len() {
+        let a = &args[i];
+        i += 1;
+        match a.as_str() {
+            "--app-id" => {
+                app_id = args[i].as_str();
+                i += 1;
+            }
+            "--help" => {
+                usage(exe);
+                std::process::exit(0);
+            }
+            _ => {
+                args_gtk.push(a.clone());
             }
         }
-        gtk::glib::Propagation::Proceed
+    }
+
+    let app = gtk::Application::builder().application_id(app_id).build();
+    app.connect_activate(build_window);
+    app.connect_startup(add_css);
+    app.run_with_args(&args_gtk)
+}
+
+fn add_css(_: &gtk::Application) {
+    let css_provider = CssProvider::new();
+    css_provider.load_from_data(include_str!("default-style.css"));
+
+    let user_css_path = dirs::config_dir().unwrap().join("blankwindow.css");
+    if let Ok(user_css) = fs::read_to_string(user_css_path) {
+        css_provider.load_from_data(user_css.as_str());
+    };
+
+    gtk::style_context_add_provider_for_display(
+        &gdk::Display::default().unwrap(),
+        &css_provider,
+        800,
+    );
+}
+
+fn build_window(app: &gtk::Application) {
+    let window = gtk::ApplicationWindow::builder().application(app).build();
+    let vbox = gtk::Box::new(Vertical, 0);
+
+    let key_ctrl = EventControllerKey::builder()
+        .propagation_phase(gtk::PropagationPhase::Capture)
+        .build();
+    let app_clone = app.clone();
+    key_ctrl.connect_key_pressed(move |_, keyval, _, modifiers| {
+        if modifiers == ModifierType::CONTROL_MASK && keyval == gdk::Key::Q {
+            app_clone.quit();
+        }
+        glib::Propagation::Stop
     });
 
-    window.add(&vbox);
-
-    window.show_all();
-
-    gtk::main();
+    window.add_controller(key_ctrl);
+    window.set_child(Some(&vbox));
+    window.present();
 }
